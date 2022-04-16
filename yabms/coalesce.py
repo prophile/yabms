5714e924b976112d8f50da18423351d7c16811ce
Alistair Lynn <arplynn@gmail.com>
2022-04-16 10:24:57 +0000

    Enforce a facing constraint at the coalescing stage

--- yabms/coalesce.py
+++ yabms/coalesce.py
@@ -5,6 +5,7 @@ Having built a valid proto-round, we can coalesce multiple
 proto-rounds together to build a single schedule.
 """
 
+import itertools
 import random
 import sys
 
@@ -12,6 +13,8 @@ import tqdm
 
 
 def _is_valid(provisional, *, spacing=1):
+    all_teams = sorted({y for x in provisional for y in x})
+
     # Check the spacing constraint
     zone_count = len(provisional[0])
     for window_start in range(len(provisional) - spacing):
@@ -19,6 +22,21 @@ def _is_valid(provisional, *, spacing=1):
         teams = {y for x in window for y in x}
         if len(teams) != zone_count * (spacing + 1):
             return False
+
+    # Check the max-facing constraint
+    facings = {
+        (x, y): 0 for n, x in enumerate(all_teams[:-1]) for y in all_teams[n + 1 :]
+    }
+
+    for match in provisional:
+        for left_team, right_team in itertools.combinations(sorted(match), 2):
+            facings[left_team, right_team] += 1
+
+    highest_facing = max(facings.values())
+    lowest_facing = min(facings.values())
+
+    if highest_facing - lowest_facing > 2:
+        return False
 
     return True
 
