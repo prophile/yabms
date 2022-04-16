247107d4a370e4fe7a4b21934b0aa1c97b644b37
Alistair Lynn <arplynn@gmail.com>
2022-04-16 11:08:30 +0000

    Use a constraint solver for coalescing

--- yabms/coalesce.py
+++ yabms/coalesce.py
@@ -5,63 +5,74 @@ Having built a valid proto-round, we can coalesce multiple
 proto-rounds together to build a single schedule.
 """
 
-import itertools
-import random
 import sys
 
 import tqdm
-
-
-def _is_valid(provisional, *, spacing=1):
-    all_teams = sorted({y for x in provisional for y in x})
-
-    # Check the spacing constraint
-    zone_count = len(provisional[0])
-    for window_start in range(len(provisional) - spacing):
-        window = provisional[window_start : window_start + spacing + 1]
-        teams = {y for x in window for y in x}
-        if len(teams) != zone_count * (spacing + 1):
-            return False
-
-    # Check the exact duplicate constraint
-    match_combinations = {tuple(sorted(match)) for match in provisional}
-    if len(match_combinations) != len(provisional):
-        return False
-
-    # Check the max-facing constraint
-    facings = {
-        (x, y): 0 for n, x in enumerate(all_teams[:-1]) for y in all_teams[n + 1 :]
-    }
-
-    for match in provisional:
-        for left_team, right_team in itertools.combinations(sorted(match), 2):
-            facings[left_team, right_team] += 1
-
-    highest_facing = max(facings.values())
-    lowest_facing = min(facings.values())
-
-    if highest_facing - lowest_facing > 2:
-        return False
-
-    return True
+import z3
 
 
 def _add_round(proto_round, confirmed, *, spacing=1):
     num_teams = len({y for x in proto_round for y in x})
     teams = list(range(num_teams))
 
-    # For each round, we need to select a team assignment of
-    # the proto-round which preserves the spacing window.
-    for _ in range(1_000_000):
-        random.shuffle(teams)
+    solver = z3.Solver()
 
-        provisional = list(confirmed)  # Copy the confirmed list
-        this_round = [[teams[n] for n in match] for match in proto_round]
-        provisional += this_round
-        if _is_valid(provisional):
-            return provisional
-    else:
-        raise ValueError("Could not find a valid schedule.")
+    team_assignment = {team: z3.Int(f"team-{team}") for team in teams}
+
+    # 1: Range constraints. Each assignment must be a team number.
+    for assignment in team_assignment.values():
+        solver.add(assignment >= min(teams))
+        solver.add(assignment <= max(teams))
+
+    # 2: Bijection constraints. Each team must appear exactly once.
+    values = list(team_assignment.values())
+    for ix, left_assignment in enumerate(values[:-1]):
+        for right_assignment in values[ix + 1 :]:
+            solver.add(left_assignment != right_assignment)
+    del values
+
+    # 3: Spacing constraints. Teams must have at least the spacing gap
+    # between appearances. We need only implement this for the first
+    # few matches, as the spacing within the proto-round is already
+    # enforced.
+    for ix, match in enumerate(proto_round[:spacing]):
+        conflicting_teams = {
+            team for true_match in confirmed[-spacing + ix :] for team in true_match
+        }
+        for pseudo_team in match:
+            for conflicting_team in conflicting_teams:
+                solver.add(team_assignment[pseudo_team] != conflicting_team)
+
+    # 4: Duplication constraints. The assignment cannot produce any fully
+    # duplicated matches.
+    for true_match in confirmed:
+        for provisional_match in proto_round:
+            match_terms = []
+            for true_team, pseudo_team in zip(true_match, provisional_match):
+                match_terms.append(team_assignment[pseudo_team] == true_team)
+            exact_match = z3.And(*match_terms)
+            solver.add(z3.Not(exact_match))
+
+    # TODO: 5: Facing constraints.
+
+    result = solver.check()
+
+    if result != z3.sat:
+        raise ValueError("Solver failed to find a team assignment.")
+
+    model = solver.model()
+
+    true_team_assignments = {
+        pseudo_team: model[team_assignment[pseudo_team]].as_long()
+        for pseudo_team in team_assignment
+    }
+
+    new_round = [
+        [true_team_assignments[pseudo_team] for pseudo_team in match]
+        for match in proto_round
+    ]
+
+    return confirmed + new_round
 
 
 def coalesce(proto_round, num_rounds, *, spacing=1):
