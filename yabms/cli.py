9de53f39e670b3492bd4986cfd6056201fe704a1
Alistair Lynn <arplynn@gmail.com>
2022-04-19 19:06:22 +0000

    Add 'just rebalance' mode

--- yabms/cli.py
+++ yabms/cli.py
@@ -53,25 +53,36 @@ def argument_parser():
         dest="balance",
         help="do not balance the teams",
     )
+    parser.add_argument(
+        "--rebalance",
+        type=argparse.FileType("r"),
+        help="rebalance the teams from a file",
+    )
     return parser
 
 
 def main(args=sys.argv[1:]):
     """Run as main entry point."""
     options = argument_parser().parse_args(args)
-    pr = protoround.build_proto_round(
-        num_teams=options.num_teams,
-        appearances_per_round=options.appearances,
-        num_zones=options.zones,
-        spacing=options.spacing,
-    )
-    # print("PR", pr)
-    fs = coalesce.coalesce(
-        pr,
-        num_rounds=options.rounds,
-        spacing=options.spacing,
-    )
-    # print(fs)
+
+    if options.rebalance:
+        fs = []
+        for line in options.rebalance:
+            fs.append([int(x) - 1 for x in line.split("|")])
+    else:
+        pr = protoround.build_proto_round(
+            num_teams=options.num_teams,
+            appearances_per_round=options.appearances,
+            num_zones=options.zones,
+            spacing=options.spacing,
+        )
+        # print("PR", pr)
+        fs = coalesce.coalesce(
+            pr,
+            num_rounds=options.rounds,
+            spacing=options.spacing,
+        )
+        # print(fs)
     if options.balance:
         final = permute.permute_zones(fs)
     else:
